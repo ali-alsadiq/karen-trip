70391f40f94c158832daf57b2d973a12302039fe
Ali Alsaadiq <81187117+ali-alsadiq@users.noreply.github.com>
2023-05-07 00:13:58 +0300

    Initial commit: Set up Xcode project for Karen Trip app with basic storyboard structure and initial files.
    The first commit adds the initial project files for Karen Trip, an iOS app for planning trips and booking hotels, created using Xcode and Swift.

---
+++ KarenTrip/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KarenTrip
+//
+//  Created by Ali Alsadiq on 2023-05-06.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
